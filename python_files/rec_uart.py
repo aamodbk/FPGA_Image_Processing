7bd06825613090a74405625968174811a1da4aca
Aamod B K <77502673+aamodbk@users.noreply.github.com>
2023-11-10 01:59:45 +0530

    Add files via upload

---
+++ python_files/rec_uart.py
@@ -0,0 +1,18 @@
+import serial           # import the module
+import struct
+import time
+
+ComPort = serial.Serial('COM6') # open COM24
+ComPort.baudrate = 115200 # set Baud rate to 9600
+ComPort.bytesize = 7    # Number of data bits = 7
+ComPort.parity   = 'N'  # No parity
+ComPort.stopbits = 1    # Number of Stop bits = 1
+
+N_RECEIVED_BYTES = 1
+
+while True:
+    it=ComPort.read(N_RECEIVED_BYTES)                #for receiving data from FPGA
+
+    print(f"Received data: {int.from_bytes(it, byteorder='big')}")
+
+ComPort.close()         # Close the Com port
